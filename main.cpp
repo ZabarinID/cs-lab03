74eb1440381b2c4fe82599fd200d3ad8928a7b55
ZabarinID <iliya.zabarin.I@gmail.com>
2019-04-02 17:15:55 +0300

    code: Тесты

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include "histogram.h"
 using namespace std;
 
 vector<double>
@@ -15,23 +15,6 @@ input_numbers(size_t count)
     return result;
 }
 
-void
-find_minmax(vector<double> numbers, double& min, double& max)
-{
-    min = numbers[0];
-    max = numbers[0];
-    for (double number : numbers)
-    {
-        if (number < min)
-        {
-            min = number;
-        }
-        if (number > max)
-        {
-            max = number;
-        }
-    }
-}
 
 vector<size_t>
 make_histogram(const vector<double> &numbers, size_t bin_count)
