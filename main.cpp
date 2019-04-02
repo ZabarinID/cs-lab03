f421c779444190b97d080484d46b1400407213fc
ZabarinID <iliya.zabarin.I@gmail.com>
2019-04-02 15:00:33 +0300

    code: Передача по константной ссылке

--- main.cpp
+++ main.cpp
@@ -4,31 +4,99 @@
 using namespace std;
 
 vector<double>
-input_numbers(size_t count) {
+input_numbers(size_t count)
+{
     vector<double> result(count);
     cerr << "Enter numbers: ";
-    for (size_t i = 0; i < count; i++) {
+    for (size_t i = 0; i < count; i++)
+    {
         cin >> result[i];
     }
     return result;
 }
 
 void
-find_minmax(vector<double> numbers, double& min, double& max) {
+find_minmax(vector<double> numbers, double& min, double& max)
+{
     min = numbers[0];
     max = numbers[0];
-    for (double number : numbers) {
-        if (number < min) {
+    for (double number : numbers)
+    {
+        if (number < min)
+        {
             min = number;
         }
-        if (number > max) {
+        if (number > max)
+        {
             max = number;
         }
     }
 }
 
+vector<size_t>
+make_histogram(const vector<double> &numbers, size_t bin_count)
+{
+    double min, max;
+    find_minmax(numbers, min, max);
+    vector<size_t> bins (bin_count);
+    for (double number : numbers)
+    {
+        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
+        if (bin == bin_count)
+        {
+            bin--;
+        }
+        bins[bin]++;
+    }
+    return bins;
+}
+
+void
+show_histogram_text(const vector<double> &numbers, size_t bin_count, vector<size_t> &bins)
+{
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+
+    size_t max_count = 0;
+    for (size_t count : bins)
+    {
+        if (count > max_count)
+        {
+            max_count = count;
+        }
+    }
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+
+    for (size_t bin : bins)
+    {
+        if (bin < 100)
+        {
+            cout << ' ';
+        }
+        if (bin < 10)
+        {
+            cout << ' ';
+        }
+        cout << bin << "|";
+
+        size_t height = bin;
+        if (scaling_needed)
+        {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            height = (size_t)(bin * scaling_factor);
+        }
+
+        for (size_t i = 0; i < height; i++)
+        {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+}
+
 int
-main() {
+main()
+{
     // Ввод данных
 
     size_t number_count;
@@ -42,50 +110,12 @@ main() {
     cin >> bin_count;
 
     // Обработка данных
-    double min, max;
-    find_minmax(numbers, min, max);
 
-    vector<size_t> bins(bin_count);
-    for (double number : numbers) {
-        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
-        if (bin == bin_count) {
-            bin--;
-        }
-        bins[bin]++;
-    }
+    vector<size_t> bins = make_histogram (numbers, bin_count);
+
 
     // Вывод данных
-    const size_t SCREEN_WIDTH = 80;
-    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
-
-    size_t max_count = 0;
-    for (size_t count : bins) {
-        if (count > max_count) {
-            max_count = count;
-        }
-    }
-    const bool scaling_needed = max_count > MAX_ASTERISK;
-
-    for (size_t bin : bins) {
-        if (bin < 100) {
-            cout << ' ';
-        }
-        if (bin < 10) {
-            cout << ' ';
-        }
-        cout << bin << "|";
-
-        size_t height = bin;
-        if (scaling_needed) {
-            const double scaling_factor = (double)MAX_ASTERISK / max_count;
-            height = (size_t)(bin * scaling_factor);
-        }
-
-        for (size_t i = 0; i < height; i++) {
-            cout << '*';
-        }
-        cout << '\n';
-    }
+    show_histogram_text(numbers, bin_count, bins);
 
     return 0;
 }
